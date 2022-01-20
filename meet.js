2f48fdf16b06fc909cffac0a46dfe255806cf369
Russo Salvatore <russo.salvatore@osdbsesto>
2022-01-20 11:10:55 +0100

    program /registrazione

--- meet.js
+++ meet.js
@@ -1,13 +1,18 @@
-const{response,request}=require("express");
+const{response}=require("express");
 var express=require("express");
 var apiserver=express();
 var fs=require("fs");
 
 console.log("api in function");
+
 var port=3000;
 var host="localhost";
 apiserver.listen(port,host,()=>{
     console.log("server running at http://%s%d",host,port);
+});
+apiserver.get("/",(request,response)=>{
+    console.log("sono in GET",request);
+    response.send("ciao client sei in home");
 });
 apiserver.post("/registrazione",(request,response)=>{
 console.log("insert student "+request.body.user+" and "+request.body.password);
@@ -16,7 +21,7 @@ fs.writeFile("studenti.js",scrivere,err=>{
     if(err){
          console.log("error:"+err);
     }else{
-console.log("student save");
+response.send("student save");
 }
 })
 });
@@ -33,5 +38,6 @@ apiserver.post("/control",(request,response)=>{
                 response.send("lo studente è: "+students[a].id+" "+students[a].surname+" "+students[a].name); 
             }
         }
-    })
+    }
+})
 });
