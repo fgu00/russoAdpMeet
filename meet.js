bdb61ff3ee6f9f7c2f2290a6851e3dc00be2a5a9
Russo Salvatore <russo.salvatore@osdbsesto>
2022-01-20 10:48:17 +0100

    add /registrazione

---
+++ meet.js
@@ -0,0 +1,37 @@
+const{response,request}=require("express");
+var express=require("express");
+var apiserver=express();
+var fs=require("fs");
+
+console.log("api in function");
+var port=3000;
+var host="localhost";
+apiserver.listen(port,host,()=>{
+    console.log("server running at http://%s%d",host,port);
+});
+apiserver.post("/registrazione",(request,response)=>{
+console.log("insert student "+request.body.user+" and "+request.body.password);
+var scrivere=request.body.user+","+request.body.password;
+fs.writeFile("studenti.js",scrivere,err=>{
+    if(err){
+         console.log("error:"+err);
+    }else{
+console.log("student save");
+}
+})
+});
+apiserver.post("/control",(request,response)=>{          
+    console.log("student "+request.body.user+" and "+request.body.password);
+    //leggere file
+    sf.readFile("studenti.js",(err,data)=>{
+        if(err){ console.log("error:"+err);
+    }else{
+        var students=JSON.parse(data);
+        console.log("students:"+students[0].surname);
+        for(var a=0;a<students.length;a++){
+            if(students[a].id==request.query.id){
+                response.send("lo studente è: "+students[a].id+" "+students[a].surname+" "+students[a].name); 
+            }
+        }
+    })
+});
